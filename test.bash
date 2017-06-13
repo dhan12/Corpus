cfe9bae69b739d295807c4175e57558bec5acc0e
dhan12 <emaildhan@gmail.com>
2017-06-13 07:11:01 -0400

    Add arguments to test.bash

--- test.bash
+++ test.bash
@@ -1,13 +1,41 @@
 #!/bin/bash
 echo "start"
 
-virtualenv venv
-. venv/bin/activate
+
+function start_virtualenv {
+    virtualenv venv
+    . venv/bin/activate
+}
+
+function stop_virtualenv {
+    deactivate
+}
+
+# Check arguments
+if [[ $# -gt 0 ]]; then
+    if [[ "$1" = "install" ]]; then
+        echo "Will install packages"
+        start_virtualenv
+        pip install pytest
+        pip install pep8
+        pip install autopep8
+    elif [[ "$1" = "autopep8" ]]; then
+        echo "Will run autopep8"
+        start_virtualenv
+        autopep8 --in-place *.py tests/*.py
+    else
+        echo "Unknown argument: $1"
+    fi
+
+    stop_virtualenv
+    exit
+fi
+
 
 #
 # Unit tests
 #
-pip install pytest
+start_virtualenv
 python -m pytest tests
 rc=$?
 
@@ -15,16 +43,11 @@ rc=$?
 # Do some style checks
 #
 if [ $rc -eq 0 ]; then
-    pip install pep8
     venv/bin/pep8 *.py tests
     rc=$?
 fi
 
-if [ 0 -eq 1 ]; then
-    pip install autopep8
-    autopep8 --in-place *.py tests/*.py
-fi
 
-deactivate
+stop_virtualenv
 
 exit $rc
